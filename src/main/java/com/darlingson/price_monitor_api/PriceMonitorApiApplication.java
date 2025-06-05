07cbb4cc80a9cc5a943426aa1c0bb4ef988475b9
darlingson <dmakuwila@gmail.com>
2025-06-05 12:44:51 +0200

    initial project commit

---
+++ src/main/java/com/darlingson/price_monitor_api/PriceMonitorApiApplication.java
@@ -0,0 +1,13 @@
+package com.darlingson.price_monitor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceMonitorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceMonitorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darlingson/price_monitor_api/PriceMonitorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darlingson.price_monitor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceMonitorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
